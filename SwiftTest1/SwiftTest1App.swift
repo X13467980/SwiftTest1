4eac71b4f9f33dae0e7316ec4791ecb45631c2d2
Youta <0x59.xxx@gmail.com>
2024-12-21 23:43:35 +0900

    Initial Commit

---
+++ SwiftTest1/SwiftTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTest1App.swift
+//  SwiftTest1
+//
+//  Created by X on 2024/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTest1Tests/SwiftTest1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTest1Tests.swift
+//  SwiftTest1Tests
+//
+//  Created by X on 2024/12/21.
+//
+
+import Testing
+@testable import SwiftTest1
+
+struct SwiftTest1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
